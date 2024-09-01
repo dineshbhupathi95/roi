af12f158e9f1c0667606a4d90eacb434c6701ec7
Dinesh Bhupathi <dineshbhupathi@Dineshs-MacBook-Air.local>
2024-09-01 19:47:47 +0530

    api change

--- UI/src/AppConfig.js
+++ UI/src/AppConfig.js
@@ -1,8 +1,8 @@
 let local = 'http://localhost:8000'
-let server = ''
+let server = 'http://65.0.21.44:8000'
 
 const apiConfig = {
-    baseURL: local
+    baseURL: server
 }
 
 export default apiConfig;
